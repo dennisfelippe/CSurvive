42e94c25586e416a44fe9ea70c44e5b0cbaa19d0
dennisfelippe <zero_dfvn@hotmail.com>
2016-04-19 15:02:03 -0300

    Versao Teste

---
+++ CT Survive.rb
@@ -0,0 +1,10 @@
+require 'gosu'
+require_relative 'scripts/window'
+require_relative 'scripts/player'
+require_relative 'scripts/enemy'
+require_relative 'scripts/enemy2'
+require_relative 'scripts/back'
+require_relative 'scripts/game_over'
+
+window = Window.new
+window.show
